7ec0447ea22b21243119dadb48515dea0033e750
atsushi0919 <hatake0919@yahoo.co.jp>
2022-08-03 17:06:43 +0900

    セクション2-1～5 Ruby解答追加

--- graph_dfs_problems/02-05_path_all_step5.rb
+++ graph_dfs_problems/02-05_path_all_step5.rb
@@ -1,12 +1,123 @@
 # パスの経由地 2 (paizaランク B 相当)
 # https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_all_step5
 
+INPUT1 = <<~"EOS"
+  5 1 4 3
+  2
+  2 5
+  3
+  1 3 5
+  3
+  2 4 5
+  2
+  3 5
+  4
+  1 2 3 4
+EOS
+OUTPUT1 = <<~"EOS"
+  5
+  1 2 3 4
+  1 2 3 5 4
+  1 2 5 3 4
+  1 5 2 3 4
+  1 5 3 4
+EOS
+
+INPUT2 = <<~"EOS"
+  5 5 3 1
+  2
+  2 5
+  3
+  1 3 5
+  3
+  2 4 5
+  2
+  3 5
+  4
+  1 2 3 4
+EOS
+OUTPUT2 = <<~"EOS"
+  1
+  5 1 2 3
+EOS
+
+INPUT3 = <<~"EOS"
+  8 8 4 1
+  1
+  2
+  7
+  1 3 4 5 6 7 8
+  6
+  2 4 5 6 7 8
+  6
+  2 3 5 6 7 8
+  6
+  2 3 4 6 7 8
+  6
+  2 3 4 5 7 8
+  6
+  2 3 4 5 6 8
+  6
+  2 3 4 5 6 7
+EOS
+OUTPUT3 = <<~"EOS"
+  0
+EOS
+
+def dfs(adjacent_list, history, t)
+  # history の末尾 から t への経路を調べる
+  results = []
+  walks = [history]
+  while walks.length > 0
+    current_walk = walks.pop
+    if current_walk.last == t
+      results << current_walk
+      next
+    end
+
+    # 未訪問の隣接頂点に移動する
+    adjacent_list[current_walk.last - 1].each do |next_node|
+      next if current_walk.include?(next_node)
+      walks << current_walk + [next_node]
+    end
+  end
+  results
+end
+
+def main(input_str)
+  input_lines = input_str.split("\n")
+  # n: 頂点数, s: 起点, t: 終点, p: 中継点
+  n, s, t, p = input_lines.shift.split.map(&:to_i)
+  # 隣接リスト
+  adjacent_list = []
+  input_lines.each_with_index do |line, idx|
+    next if idx.even?
+    adjacent_list << line.split.map(&:to_i)
+  end
+
+  # 始点 s から中継点 p までの経路
+  s_to_p = dfs(adjacent_list, [s], p)
+  # 中継点 p から終点 t までの経路
+  results = []
+  s_to_p.each do |walk|
+    tmp = dfs(adjacent_list, walk, t)
+    results.concat(tmp) if tmp.length > 0
+  end
+  # 経路数と経路を全て出力
+  [results.length.to_s].concat(results.map { |e| e.join(" ") }).join("\n")
+end
+
+puts main(STDIN.read)
+
 =begin
 問題文のURLをコピーする
  下記の問題をプログラミングしてみよう！
-1, ..., n の番号がついた n 個の頂点とそれらをつなぐ枝からなる無向グラフを考えます。ただし、自己ループと多重辺は考えません。
+1, ..., n の番号がついた n 個の頂点とそれらをつなぐ枝からなる無向グラフを考えます。
+ただし、自己ループと多重辺は考えません。
 
-隣接リストとある頂点の組 (s,t) と s, t ではない頂点 p が与えられます。このとき、頂点 s と頂点 t を端点とし、頂点 p を経由するパスを全て出力してください。ただし、ここでパスとは頂点と枝の反復を許さない経路のことを言います。
+隣接リストとある頂点の組 (s,t) と s, t ではない頂点 p が与えられます。
+このとき、頂点 s と頂点 t を端点とし、頂点 p を経由するパスを全て出力してください。
+ただし、ここでパスとは頂点と枝の反復を許さない経路のことを言います。
 
 ▼　下記解答欄にコードを記入してみよう
 
--- graph_dfs_problems/02-06_path_all_step6.rb
+++ graph_dfs_problems/02-06_path_all_step6.rb
@@ -1,12 +1,131 @@
 # パスの通れない頂点 2 (paizaランク B 相当)
 # https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_all_step6
 
+INPUT1 = <<~"EOS"
+  5 1 4
+  3
+  2 3 5
+  2
+  2 5
+  3
+  1 3 5
+  3
+  2 4 5
+  2
+  3 5
+  4
+  1 2 3 4
+EOS
+OUTPUT1 = <<~"EOS"
+  6
+  2 1 3 2 4 3
+  2 1 3 4 2 3
+  2 3 1 2 4 3
+  2 3 4 2 1 3
+  2 4 3 1 2 3
+  2 4 3 2 1 3
+EOS
+
+INPUT2 = <<~"EOS"
+  5 5 3
+  3
+  1 2 4
+  2
+  2 5
+  3
+  1 3 5
+  3
+  2 4 5
+  2
+  3 5
+  4
+  1 2 3 4
+EOS
+OUTPUT2 = <<~"EOS"
+  1
+  5 3
+EOS
+
+INPUT3 = <<~"EOS"
+  7 3 4
+  1
+  2
+  6
+  2 3 4 5 6 7
+  3
+  1 3 7
+  3
+  1 2 4
+  3
+  1 3 5
+  3
+  1 4 6
+  3
+  1 5 7
+  3
+  1 2 6
+EOS
+OUTPUT3 = <<~"EOS"
+  5
+  3 1 4
+  3 1 5 4
+  3 1 6 5 4
+  3 1 7 6 5 4
+  3 4
+EOS
+
+def dfs(adjacent_list, history, t, non_visits = [])
+  # history の末尾 から t への経路を調べる
+  results = []
+  walks = [history]
+  while walks.length > 0
+    current_walk = walks.pop
+    if current_walk.last == t
+      results << current_walk
+      next
+    end
+
+    # 未訪問の隣接頂点に移動する
+    adjacent_list[current_walk.last - 1].each do |next_node|
+      next if current_walk.include?(next_node)
+      next if non_visits.include?(next_node)
+      walks << current_walk + [next_node]
+    end
+  end
+  results
+end
+
+def main(input_str)
+  input_lines = input_str.split("\n")
+  # n: 頂点数, s: 起点, t: 終点
+  n, s, t = input_lines.shift.split.map(&:to_i)
+  # 通らないノード
+  non_visits = input_lines.shift(2).last.split.map(&:to_i)
+  # 隣接リスト
+  adjacent_list = []
+  input_lines.each_with_index do |line, idx|
+    next if idx.even?
+    adjacent_list << line.split.map(&:to_i)
+  end
+
+  # 始点 s から終点 t までの経路
+  results = dfs(adjacent_list, [s], t, non_visits)
+
+  # 経路数と経路を全て出力
+  [results.length.to_s].concat(results.map { |e| e.join(" ") }).join("\n")
+end
+
+puts main(STDIN.read)
+
 =begin
 問題文のURLをコピーする
  下記の問題をプログラミングしてみよう！
-1, ..., n の番号がついた n 個の頂点とそれらをつなぐ枝からなる無向グラフを考えます。ただし、自己ループと多重辺は考えません。
+1, ..., n の番号がついた n 個の頂点とそれらをつなぐ枝からなる無向グラフを考えます。
+ただし、自己ループと多重辺は考えません。
 
-隣接リストとある頂点の組 (s,t) と s,t を含まない頂点の集合 S が与えられます。このとき、頂点 s と頂点 t を端点とし、S の頂点を通らないパスを全て出力してください。ただし、ここでパスとは頂点と枝の反復を許さない経路のことを言います。
+隣接リストとある頂点の組 (s,t) と s,t を含まない頂点の集合 S が与えられます。
+このとき、頂点 s と頂点 t を端点とし、S の頂点を通らないパスを全て出力してください。
+ただし、ここでパスとは頂点と枝の反復を許さない経路のことを言います。
 
 ▼　下記解答欄にコードを記入してみよう
 
@@ -54,7 +173,7 @@ a_1 ... a_{v_n}
 ・ 1 ≦ a_j < a_{j+1} ≦ n (1 ≦ j ≦ (v_i)-1, 1 ≦ i ≦ n)
 
 入力例1
-5 1 4
+5 2 3
 3
 2 3 5
 2
