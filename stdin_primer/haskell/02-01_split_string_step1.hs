77f5c40fa95d6b447a22a77ac389b8e182ee1b7c
atsushi0919 <hatake0919@yahoo.co.jp>
2022-10-31 08:45:29 +0900

    セクション2 Haskell 解答追加

--- stdin_primer/haskell/02-01_split_string_step1.hs
+++ stdin_primer/haskell/02-01_split_string_step1.hs
@@ -1,28 +1,10 @@
-{-
-2 つの文字列の半角スペース区切りでの分割 (paizaランク D 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+-- 2 つの文字列の半角スペース区切りでの分割 (paizaランク D 相当)
+-- https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_string_step1
 
-シェア用URL:
-https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_string_step1
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-文字列Hello paizaを、半角スペースで分割して出力してください。
+main :: IO()
+main = do
+  let inputStr = "Hello paiza"
+  let splittedWords = words inputStr
 
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-なし
-
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-Hello paizaを半角スペースで区切り、区切られた文字列を改行区切りにして 2 行で出力してください。
-また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-
-Hello
-paiza
-条件
-なし
--}
+  putStrLn $ head splittedWords
+  putStrLn $ last splittedWords
--- stdin_primer/haskell/02-03_split_string_boss.hs
+++ stdin_primer/haskell/02-03_split_string_boss.hs
@@ -1,31 +1,9 @@
-{-
-【半角スペース区切りでの文字列の分割】5 つの文字列の半角スペース区切りでの分割 (paizaランク D 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+-- 【半角スペース区切りでの文字列の分割】5 つの文字列の半角スペース区切りでの分割 (paizaランク D 相当)
+-- https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_string_boss
 
-シェア用URL:
-https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_string_boss
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-文字列one two three four fiveを、半角スペースで分割して出力してください。
+main :: IO()
+main = do
+  let inputStr = "one two three four five"
+  let splittedWords = words inputStr
 
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-なし
-
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-one two three four fiveを半角スペースで区切り、区切られた文字列を改行区切りにして 5 行で出力してください。
-また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-
-one
-two
-three
-four
-five
-条件
-なし
--}
+  putStrLn $ unlines splittedWords
