44f036fe35646b96a93364eadf62c6688b4251bc
atsushi0919 <hatake0919@yahoo.co.jp>
2022-10-31 14:24:33 +0900

    セクション3 Haskell 解答追加

--- stdin_primer/haskell/03-04_split_input_step4.hs
+++ stdin_primer/haskell/03-04_split_input_step4.hs
@@ -1,82 +1,8 @@
-{-
-半角スペース区切りの 10 個の入力 (paizaランク D 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+-- 半角スペース区切りの 10 個の入力 (paizaランク D 相当)
+-- https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step4
 
-シェア用URL:
-https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step4
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-文字列 s_1, s_2, ... s_9, s_10 が半角スペースで区切られて 1 行で与えられます。
-各文字列を出力するごとに改行し 10 行で出力してください。
+main :: IO ()
+main = do
+  inputWords <- words <$> getLine
 
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-以下の形式で標準入力によって与えられます。
-文字列 s_1, s_2, ... s_9, s_10 が半角スペースで区切られて 1 行で与えられます。
-
-
-s_1 s_2 ... s_9 s_10
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-入力された文字列 s_1, s_2, ... s_9, s_10 をそれぞれ改行区切りで出力してください。
-また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-
-s_1
-s_2
-...
-s_9
-s_10
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-・s_1, s_2, ... s_9, s_10 は数字または大文字・小文字のアルファベットからなる長さ 1 以上 10 以下の文字列
-
-入力例1
-VypymzW25l 3xsXWAK NYwbs2MRC kh4 mr38o8W9 94 fEJ 6THlW fJFTy n
-
-出力例1
-VypymzW25l
-3xsXWAK
-NYwbs2MRC
-kh4
-mr38o8W9
-94
-fEJ
-6THlW
-fJFTy
-n
-
-入力例2
-VDHUoNUym oqnbK7XsHK GPuafB yWdzgE bUMaMdBV 3kDU9 AbWnot 1E44Ekwz jdu Y0e
-
-出力例2
-VDHUoNUym
-oqnbK7XsHK
-GPuafB
-yWdzgE
-bUMaMdBV
-3kDU9
-AbWnot
-1E44Ekwz
-jdu
-Y0e
-
-入力例3
-F5G6 0KSYzlF ghYYhAw NHCSxgPMbE EYqKRElk 8UYzUH 6mxtW 0tt1n 2cEaAQs gAWn
-
-出力例3
-F5G6
-0KSYzlF
-ghYYhAw
-NHCSxgPMbE
-EYqKRElk
-8UYzUH
-6mxtW
-0tt1n
-2cEaAQs
-gAWn
--}
+  putStrLn (unlines inputWords)
